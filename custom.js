7ed9972e723fcabe162e895b4303768955f8d091
Alex Prud'homme <alex.prudhomme.1@ulaval.ca>
2021-12-21 21:46:00 -0500

    added title added icon added tabs added switch between tabs

--- custom.js
+++ custom.js
@@ -1,9 +1,11 @@
 //clear
+
 document.querySelector('#clear').addEventListener('click', ()=> {
-   document.querySelector("#display").value = ' ';
+   document.querySelector("#display").value = " ";
 })
 // backspace function
 const backSpace = () =>{
     const num = document.querySelector("#display").value.slice(0, -1)
     document.querySelector("#display").value = num
 }
+//activate cours 1
